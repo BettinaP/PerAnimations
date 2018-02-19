86d247ef82bea36ba75de2da3664a8d25397454c
BettinaP <bettinaprophete@gmail.com>
2018-02-18 21:32:58 -0500

    working on fixing image skipping during animation

--- PerAnimations/ViewController.swift
+++ PerAnimations/ViewController.swift
@@ -18,39 +18,50 @@ class ViewController: UIViewController {
     var isAnimating = false
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         imageView.image = UIImage(named:"frame_0_delay-0.07s.gif")
         
     }
     
     @objc func animate() {
-        
-        imageView.image = UIImage(named:"frame_\(counter)_delay-0.07s.gif")
-        counter += 1
+            if counter < 9 {
+                imageView.image = UIImage(named:"frame_0\(counter)_delay-0.07s.gif")
+                counter += 01
+                print("frame_0\(counter)_delay-0.07s.gif")
+            } else {
+                imageView.image = UIImage(named:"frame_\(counter)_delay-0.07s.gif")
+                counter += 01
+                print("else frame: frame_\(counter)_delay-0.07s.gif")
+                
+            }
+
         
         if counter == 40 {
-            
             counter = 0
-            
+            print("counter value is \(counter) and has been reset to 0 ")
         }
     }
     
     @IBAction func startAnimationTapped(_ sender: UIButton) {
         
+        
         if isAnimating {
-            
             timer.invalidate()
             animationButton.setTitle("Start Animation", for: .normal)
             isAnimating = false
-            
         } else {
-            
             timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector:#selector(ViewController.animate), userInfo: nil, repeats: true)
             animationButton.setTitle("Stop Animation", for: .normal)
             isAnimating = true
-        
         }
+        
+        //        imageView.image = UIImage(named:"frame_\(counter)_delay-0.07s.gif")
+        //        counter += 01
+        //        if counter == 40 {
+        //            counter = 0
+        //        }
+        //        print("counter: \(counter)")
+        //
     }
     
 }
